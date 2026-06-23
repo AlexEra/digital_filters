95c5ea318b607bee79346869747271c79a5f8a34
AlexEra <arcana.andromedy@gmail.com>
2026-06-24 00:31:07 +0300

    fix: step cycles; add todo

--- fir.hpp
+++ fir.hpp
@@ -17,7 +17,7 @@ concept fir_value_t = requires (T value) {
 template <
   fir_value_t T, fir_value_t V,
   size_t ImpulseCharSize, size_t WindowSize
->
+> // TODO: add `requires` to window and impulse characherestics sizes
 class FirWindowedRuntimeConvolve final {
 public:
   void set_impulse_characteristic(
@@ -30,16 +30,16 @@ public:
     y_out = 0;
     for (long j = counter, k = 0; (j != -1) && (k < WindowSize); j--, k++) {
       y_out += window[j] * h[k];
-      if (is_window_filled) {
-        for (
-          long x = 1, y = WindowSize - 1;
-          (x < WindowSize) && (y != -1);
-          x++, y--
-        ) {
-          y_out += window[x] * h[y];
-        }
-        is_window_filled = false;
+    }
+    if (is_window_filled) {
+      for (
+        long x = 1, y = WindowSize - 1;
+        (x < WindowSize) && (y != -1);
+        x++, y--
+      ) {
+        y_out += window[x] * h[y];
       }
+      is_window_filled = false;
     }
     if (++counter >= WindowSize) {
       counter = 0;
